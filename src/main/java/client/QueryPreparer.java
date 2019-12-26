d61cb220ce714fea4e8c7f4705243ab7281b8f52
Jonathan Ou <jonathanou@jonathans-mbp.home>
2019-12-26 15:02:05 -0500

    query preparer done

--- src/main/java/client/QueryPreparer.java
+++ src/main/java/client/QueryPreparer.java
@@ -10,6 +10,12 @@ public class QueryPreparer {
     private static String CONTENT_TYPE= "Content-Type";
     private static String JSON_RES = "application/json";
 
+    /**
+     * Prepare a query before being processed
+     * @param req - request of query
+     * @param res - response of query
+     */
+
     public static void prepare(Request req, Response res) {
         res.header(CONTENT_TYPE, JSON_RES);
     }
